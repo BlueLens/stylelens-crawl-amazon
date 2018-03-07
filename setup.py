40b2cac62559a5e354635fc15b7852b7852e0220
Youngbok Yoon <master@bluehack.net>
2018-03-07 11:03:49 +0900

    [STYL-227] Removed Variations from ResponseGroup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "stylelens-crawl-amazon"
-VERSION = "0.0.37"
+VERSION = "0.0.38"
 # To install the library, run the following
 #
 # python setup.py install
